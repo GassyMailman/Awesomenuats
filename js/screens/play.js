271bb3b0074cd06154295ca71920b25962ee3db7
Hayden Guthrie <seehaydeng@gmail.com>
2015-01-23 11:07:51 -0800

    play.js commented

--- js/screens/play.js
+++ js/screens/play.js
@@ -1,26 +1,30 @@
 game.PlayScreen = me.ScreenObject.extend({
-	
+	/**
+	 *  action to perform on state change
+	 */
 	onResetEvent: function() {
-		
+		// reset the score
 		game.data.score = 0;
 
 
-
+		//loads level
 		me.levelDirector.loadLevel("level01");
-	
+		//pulls the player entity from the pool
 		var player = me.pool.pull("player", 0, 420, {});
-	
+		//adds him to the game and sets his layer-level
 		me.game.world.addChild(player, 5);
 
-		
+		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
 	},
 
 
-	
+	/**
+	 *  action to perform when leaving this screen (state change)
+	 */
 	onDestroyEvent: function() {
-	
+		// remove the HUD from the game world
 		me.game.world.removeChild(this.HUD);
 	}
 });
